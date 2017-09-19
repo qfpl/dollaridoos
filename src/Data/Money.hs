a22fdaaf5ffe8376e57b4b0fd5074ec6edbe69d0
George Wilson <george@wils.online>
2017-09-19 16:09:11 +1000

    Document more

--- src/Data/Money.hs
+++ src/Data/Money.hs
@@ -1,9 +1,13 @@
 {-# LANGUAGE RankNTypes #-}
 
+-- | A data type for monetary values, with associated operations and
+--   only sensible instances.
 module Data.Money
   (
     Money(Money)
+  -- * Optics
   , money
+  -- * Operators
   , ($+$)
   , ($-$)
   , (*$)
@@ -22,12 +26,12 @@ import Data.Profunctor (Profunctor, dimap)
 import Data.Semigroup (Semigroup, (<>))
 import Data.Traversable (Traversable (traverse))
 
--- | A representation of monetary values.
+-- | A newtype for monetary values represented as type @num@.
 --
---   The @Semigroup@ instance allows amounts of money to be added together.
+--   The 'Semigroup' instance allows amounts of money to be added together.
 --
---   Any num instances present are hidden, as multiplying money by money,
---   for example, doesn't make any sense.
+--   Any 'Num' instances present are hidden, as operations like multiplying
+--   money by money don't make any sense.
 newtype Money num =
   Money num
   deriving (Eq, Ord)
@@ -57,38 +61,47 @@ type Iso s t a b = forall p f. (Profunctor p, Functor f) => p a (f b) -> p s (f 
 money :: Iso (Money a) (Money b) a b
 money = dimap (\(Money a) -> a) (fmap Money)
 
+-- | Add money to money. A synonym for @<>@.
 infixl 6 $+$
 ($+$) :: Num a => Money a -> Money a -> Money a
 ($+$) = (<>)
 
+-- | Subtract money from money
 infixl 6 $-$
 ($-$) :: Num a => Money a -> Money a -> Money a
 ($-$) (Money m) (Money n) = Money (m - n)
 
+-- | Multiply a scalar by money
 infixr 7 *$
 (*$) :: Num a => a -> Money a -> Money a
 (*$) x (Money m) = Money (x * m)
 
+-- | Multiply money by a scalar
 infixl 7 $*
 ($*) :: Num a => Money a -> a -> Money a
 ($*) = flip (*$)
 
+-- | Divide money by a scalar
 infixl 7 $/
 ($/) :: Fractional a => Money a -> a -> Money a
 ($/) (Money m) x = Money (m/x)
 
+-- | Divide money by money
 infixl 7 $/$
 ($/$) :: Fractional a => Money a -> Money a -> a
 ($/$) (Money n) (Money m) = n / m
 
+-- | Raise money to a non-negative integral power
 infixr 8 $^
 ($^) :: (Num a, Integral b) => Money a -> b -> Money a
 ($^) (Money m) x = Money (m ^ x)
 
+-- | Raise money to an integral power
 infixr 8 $^^
 ($^^) :: (Fractional a, Integral b) => Money a -> b -> Money a
 ($^^) (Money m) x = Money (m ^^ x)
 
+-- | Raise money to a floating-point power
 infixr 8 $**
 ($**) :: Floating a => Money a -> a -> Money a
 ($**) (Money m) x = Money (m ** x)
